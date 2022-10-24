629abe2dab525f63ef0d3fdee772c4e789659206
Tomas Benes <benest.radesin@gmail.com>
2022-10-24 14:03:07 +0200

    Path format redo

--- file_sync.py
+++ file_sync.py
@@ -9,7 +9,7 @@ from bs4 import SoupStrainer as ss
 
 class fileSynchronizator:
 
-    def __init__(self, url="https://portal.cisjr.cz", folder="archives"):
+    def __init__(self, url="https://portal.cisjr.cz", folder="./archives"):
 
         self.url = url
         self.folder = folder
@@ -24,7 +24,7 @@ class fileSynchronizator:
     def download_and_unzip(self, archiveName, remotePath, subFolder="", prefix = ""):
         # Get archive from ftp server
         req = requests.get(self.url+remotePath, stream=True)
-        with open("./"+archiveName, 'wb') as file:
+        with open(archiveName, 'wb') as file:
             for chunk in req.iter_content(chunk_size=128):
                 file.write(chunk)
 
@@ -34,17 +34,17 @@ class fileSynchronizator:
                 filename=archiveName, extract_dir='./tmp'
             )
         except shutil.ReadError:
-            with gzip.open("./"+archiveName, 'rb') as f_in:
+            with gzip.open(archiveName, 'rb') as f_in:
                 with open("./tmp/"+archiveName.replace(".zip",""), 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
 
         for filename in os.listdir("./tmp"):
-            shutil.move("./tmp/"+filename, "./"+self.folder+subFolder+prefix+filename)
+            shutil.move("./tmp/"+filename, self.folder+subFolder+"/"+prefix+filename)
 
 
         shutil.rmtree("./tmp")
-        if os.path.exists("./"+archiveName):
-            os.remove("./"+archiveName)
+        if os.path.exists(archiveName):
+            os.remove(archiveName)
 
     def get_all_xmls(self):
         # Get html page from url
@@ -66,7 +66,7 @@ class fileSynchronizator:
                             self.url+month_link["href"]).text, 'html.parser', parse_only=ss('a'))
                         for item in month:
                             if re.match(r'cancel.*', item.text):
-                                self.download_and_unzip(item.text, item["href"], subFolder="canceled/")
+                                self.download_and_unzip(item.text, item["href"], subFolder="/canceled")
                             elif re.match(r'.*zip', item.text):
                                 self.download_and_unzip(item.text, item["href"], prefix=month_link.text)
                     # Archive with scedule for whole year
